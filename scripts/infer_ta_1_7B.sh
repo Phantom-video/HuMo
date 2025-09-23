6a8c1022eec2d0f971b4b09ba7d5dc7d9a2664dc
LeoniusChen <clyfifa21@live.cn>
2025-09-23 21:29:28 +0800

    Fix bug, multi-gpu (2 or 4 GPUs) infer for 1.7B model

--- scripts/infer_ta_1_7B.sh
+++ scripts/infer_ta_1_7B.sh
@@ -16,13 +16,28 @@ CUDA_VISIBLE_DEVICES=0 torchrun --node_rank=0 --nproc_per_node=1 --nnodes=1 \
     generation.positive_prompt=./examples/test_case.json \
     generation.output.dir=./output
 
-
-# # Run on 8 GPUs
-# torchrun --node_rank=0 --nproc_per_node=8 --nnodes=1 \
+# # Run on 2 GPUs
+# CUDA_VISIBLE_DEVICES=0,1 torchrun --node_rank=0 --nproc_per_node=2 --nnodes=1 \
 #     --rdzv_endpoint=127.0.0.1:12345 \
 #     --rdzv_conf=timeout=900,join_timeout=900,read_timeout=900 \
 #     main.py humo/configs/inference/generate_1_7B.yaml \
-#     dit.sp_size=8 \
+#     dit.sp_size=2 \
+#     generation.frames=97 \
+#     generation.scale_t=7.0 \
+#     generation.scale_a=7.5 \
+#     generation.mode=TA \
+#     generation.height=480 \
+#     generation.width=832 \
+#     diffusion.timesteps.sampling.steps=50 \
+#     generation.positive_prompt=./examples/test_case.json \
+#     generation.output.dir=./output
+
+# # Run on 4 GPUs
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --node_rank=0 --nproc_per_node=4 --nnodes=1 \
+#     --rdzv_endpoint=127.0.0.1:12345 \
+#     --rdzv_conf=timeout=900,join_timeout=900,read_timeout=900 \
+#     main.py humo/configs/inference/generate_1_7B.yaml \
+#     dit.sp_size=4 \
 #     generation.frames=97 \
 #     generation.scale_t=7.0 \
 #     generation.scale_a=7.5 \
